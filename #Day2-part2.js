2f1a837c53e7a013d9d60fdf19323bf20cd0058e
Antonin Lesueur <87042050+Anto-LSR@users.noreply.github.com>
2023-12-01 16:10:54 +0100

    Update #Day2-part2.js

--- #Day2-part2.js
+++ #Day2-part2.js
@@ -1,46 +1,47 @@
-let sum = 0;
-const regex = new RegExp(/[^0-9]|(one|two|three|four|five|six|seven|eight|nine)/g, '')
-
 const spelledNumbers = { 'one': 'one1one', 'two': 'two2two', 'three': 'three3three', 'four': 'four4four', 'five': 'five5five', 'six': 'six6six', 'seven': 'seven7seven', 'eight': 'eight8eight', 'nine': 'nine9nine' };
-const testInput = [
-  'two1nine'
-  , 'eightwothree'
-  , 'abcone2threexyz'
-  , 'xtwone3four'
-  , '4nineeightseven2'
-  , 'zoneight234'
-  , '7pqrstsixteen']
 
-const input = fetch('https://adventofcode.com/2023/day/1/input').then(response => response.text())
-  .then(data => {
-    const dataArray = data.split('\n');
-    const numbersArray = [];
-    const replacedArray = dataArray.map(input => {
-      for (const [word, replacement] of Object.entries(spelledNumbers)) {
-        const regex = new RegExp(word, 'gi');
-        input = input.replace(regex, replacement);
-        // input = input.replace((/[^0-9]/g, ''))
-      }
-      return input;
-    });
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://adventofcode.com/2023/day/1/input');
+    const data = await response.text();
+    return data.split('\n');
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    replacedArray.forEach(line => {
-      numbersArray.push(line.replace(/[^0-9]/g, ''));
-    })
+const replaceWordsWithNumbers = (input) => {
+  for (const [word, replacement] of Object.entries(spelledNumbers)) {
+    const regex = new RegExp(word, 'gi');
+    input = input.replace(regex, replacement);
+  }
+  return input;
+};
 
-    for (const number of numbersArray) {
-      const parsedStr = number.split('');
-      try {
+const extractNumbers = (line) => {
+  return line.replace(/[^0-9]/g, '');
+};
 
-        const finalNumber = parseInt(`${parsedStr[0]}${parsedStr[parsedStr.length - 1]}`)
-        if (!isNaN(finalNumber))
-          sum += finalNumber;
-
-      } catch (e) {
-        console.log(number);
-      }
+const parseAndSumNumbers = (numbersArray) => {
+  let sum = 0;
+  numbersArray.forEach((number) => {
+    const parsedStr = number.split('');
+    try {
+      const finalNumber = parseInt(`${parsedStr[0]}${parsedStr[parsedStr.length - 1]}`);
+      if (!isNaN(finalNumber)) sum += finalNumber;
+    } catch (e) {
+      console.log(number);
     }
-    console.log(sum);
-  })
-  .catch(error => console.error(error));
+  });
+  console.log(sum);
+};
 
+const processInput = async () => {
+  let sum = 0;
+  const dataArray = await fetchData();
+  const replacedArray = dataArray.map(replaceWordsWithNumbers);
+  const numbersArray = replacedArray.map(extractNumbers);
+  parseAndSumNumbers(numbersArray);
+};
+
+processInput();
