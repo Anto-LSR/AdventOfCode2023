f9bc7085f190f3ffae107652c2186cce4ac801ca
Antonin Lesueur <87042050+Anto-LSR@users.noreply.github.com>
2023-12-04 16:13:27 +0100

    Refactor d3p2

--- day3_part2.js
+++ day3_part2.js
@@ -1,160 +1,127 @@
-
-
-///////////////////////////////////////////////////
-const inputTest = [
-    '467..114..',
-    '...*......',
-    '..35..633.',
-    '......#...',
-    '617*......',
-    '.....+.58.',
-    '..592.....',
-    '......755.',
-    '...$.*....',
-    '.664.598..']
-
+//REFACTORED VERSION
 
 const resultArray = [];
+const ratioArray = [];
 
+async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.text();
+    return data.split('\n').map(parseLine);
+}
 
-const pouet = fetch('https://adventofcode.com/2023/day/3/input').then(response => response.text()).then(data => {
-    const input = data.split('\n')
-    let firstLine = true;
-    const parsedLinesArray = []
-    for (const line of input) {
-        const regex = /(\d+)|([^.\d])/g;
-        let match;
-        const charactersWithPositions = [];
+function parseLine(line) {
+    const regex = /(\d+)|([^.\d])/g;
+    const charactersWithPositions = [];
 
-        while ((match = regex.exec(line)) !== null) {
-            if (match[1]) {
-                // C'est un nombre
-                charactersWithPositions.push({
-                    type: 'number',
-                    value: Number(match[1]),
-                    position: match.index,
-                    isPushed: false,
-                });
-            } else if (match[2]) {
-                // C'est un caractère spécial
-                charactersWithPositions.push({
-                    type: 'special',
-                    value: match[2],
-                    position: match.index,
-                    isPushed: false,
-                    associatedNumbers: [],
-                });
-            }
+    while ((match = regex.exec(line)) !== null) {
+        if (match[1]) {
+            charactersWithPositions.push({
+                type: 'number',
+                value: Number(match[1]),
+                position: match.index,
+                isPushed: false,
+            });
+        } else if (match[2]) {
+            charactersWithPositions.push({
+                type: 'special',
+                value: match[2],
+                position: match.index,
+                isPushed: false,
+                associatedNumbers: [],
+            });
         }
-
-        parsedLinesArray.push(charactersWithPositions)
     }
-    /////////////////////////LIGNE////////////////////////////
 
-    for (let i = 0; i < parsedLinesArray.length; i++) {
-        //Vérifie si la ligne comprend au moins un caractère spécial
-        const hasSpecialType = parsedLinesArray[i].some(item => item.type === 'special');
-        for (const character of parsedLinesArray[i]) {
-            //////////////VALEURS//////////////
+    return charactersWithPositions;
+}
 
-            //Récupère la position du number
-            let startPosition
-            let endPosition
-            if (character.type == 'number') {
-                if (character.value == 860) {
-                    console.log('pouet');
-                }
-                startPosition = character.position;
-                endPosition = startPosition + (character.value + '').length - 1
-                //Si un nombre est trouvé et qu'il y a un type spécial sur la ligne
+function processLines(parsedLinesArray) {
+    parsedLinesArray.forEach((line, i) => {
+        const hasSpecialType = line.some(item => item.type === 'special');
+
+        line.forEach(character => {
+            if (character.type === 'number') {
+                const startPosition = character.position;
+                const endPosition = startPosition + (character.value + '').length - 1;
+
                 if (hasSpecialType) {
-                    parsedLinesArray[i].forEach(char => {
-                        //Pour chaque type spécial
-                        if (char.type == 'special') {
-                            if (char.position === startPosition - 1 || char.position === endPosition + 1) {
-                                if (!character.isPushed) {
-                                    resultArray.push(character.value)
-                                    character.isPushed = true;
-                                    if (char.value === '*') {
-                                        char.associatedNumbers.push(character.value)
-                                    }
-                                }
-                            }
-                        }
-                    })
-                } if (!character.isPushed) {
+                    processSpecialTypes(parsedLinesArray[i], character, startPosition, endPosition);
+                }
 
-                    //Vérifier ligne précédente et suivante
-                    //Précédente
-                    if (i > 0) {
-                        const previousHasSpecialType = parsedLinesArray[i - 1].some(item => item.type === 'special');
-                        if (previousHasSpecialType) {
-                            parsedLinesArray[i - 1].forEach(char => {
-                                if (char.type == 'special') {
-                                    if (isBetween(char.position, startPosition, endPosition)) {
-                                        if (!character.isPushed) {
-                                            resultArray.push(character.value)
-                                            if (char.value === '*') {
-                                                char.associatedNumbers.push(character.value)
-                                            }
-                                        }
-                                    }
-                                }
-                            })
-                        }
-                    }
-                    //Suivante
-                    if (i < parsedLinesArray.length - 1) {
-                        const nextHasSpecialType = parsedLinesArray[i + 1].some(item => item.type === 'special');
-                        if (nextHasSpecialType) {
-                            parsedLinesArray[i + 1].forEach(char => {
-                                if (char.type == 'special') {
-                                    if (isBetween(char.position, startPosition, endPosition)) {
-                                        if (!character.isPushed) {
-                                            resultArray.push(character.value)
-                                            if (char.value === '*') {
-                                                char.associatedNumbers.push(character.value)
-                                            }
-                                        }
-                                    }
-                                }
-                            })
-                        }
-                    }
-
-
-
+                if (!character.isPushed) {
+                    checkAdjacentLines(parsedLinesArray, i, character, startPosition, endPosition);
                 }
             }
+        });
+    });
+}
+
+function processSpecialTypes(line, character, startPosition, endPosition) {
+    line.forEach(char => {
+        if (char.type === 'special' && (char.position === startPosition - 1 || char.position === endPosition + 1)) {
+            pushToResultArray(character, char);
+        }
+    });
+}
+
+function checkAdjacentLines(parsedLinesArray, index, character, startPosition, endPosition) {
+    function processAdjacentLine(adjacentLine) {
+        adjacentLine.forEach(char => {
+            if (char.type === 'special' && isBetween(char.position, startPosition, endPosition)) {
+                pushToResultArray(character, char);
+            }
+        });
+    }
+
+    if (index > 0) {
+        const previousHasSpecialType = parsedLinesArray[index - 1].some(item => item.type === 'special');
+        if (previousHasSpecialType) {
+            processAdjacentLine(parsedLinesArray[index - 1]);
         }
     }
-    console.log(resultArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0));
-    console.log(resultArray);
 
-    const ratioArray = [];
+    if (index < parsedLinesArray.length - 1) {
+        const nextHasSpecialType = parsedLinesArray[index + 1].some(item => item.type === 'special');
+        if (nextHasSpecialType) {
+            processAdjacentLine(parsedLinesArray[index + 1]);
+        }
+    }
+}
 
+function pushToResultArray(character, char) {
+    if (!character.isPushed) {
+        resultArray.push(character.value);
+        character.isPushed = true;
+
+        if (char.value === '*') {
+            char.associatedNumbers.push(character.value);
+        }
+    }
+}
+
+function calculateRatio(parsedLinesArray) {
     parsedLinesArray.forEach(line => {
         let lineRatio = 0;
         line.forEach(character => {
-            if (character.type == 'special') {
-                if (character.associatedNumbers.length === 2)
-                    lineRatio += character.associatedNumbers[0] * character.associatedNumbers[1]
-
+            if (character.type === 'special' && character.associatedNumbers.length === 2) {
+                lineRatio += character.associatedNumbers[0] * character.associatedNumbers[1];
             }
-        })
-
-        ratioArray.push(lineRatio)
-    })
+        });
+        ratioArray.push(lineRatio);
+    });
 
     console.log(ratioArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0));
-})
-
-
+}
 
 function isBetween(value, lowerBound, upperBound) {
-    //  console.log('Position : ', value, 'min : ', lowerBound, ' max : ', upperBound);
     return value >= lowerBound - 1 && value <= upperBound + 1;
 }
 
+async function main() {
+    const parsedLinesArray = await fetchData('https://adventofcode.com/2023/day/3/input');
+    processLines(parsedLinesArray);
+    console.log(resultArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0));
+    calculateRatio(parsedLinesArray);
+}
 
-
+main();
